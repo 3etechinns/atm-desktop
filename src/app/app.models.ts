6525a02bd0628b5c8745a7dfb35312cda429c99c
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-09 14:47:25 +0000

    Added App Models

--- src/app/app.models.ts
+++ src/app/app.models.ts
@@ -1,7 +1,35 @@
-export interface Region {
-  name: string;
+export interface AuthResponse {
+  access_token: string;
+  token: string;
+  expires_in: number;
 }
 
-export interface District extends Region {
-  regionID: string;
+export interface ATMData {
+  id: number;
+  name: string;
+  lat: number;
+  lng: number;
+  status: number;
+  bank_id: number;
+  city: string;
+}
+
+export interface BankData {
+  id: number;
+  name: string;
+  email: string;
+  desc: string;
+  country: string;
+  city: string;
+}
+
+export interface ATMQuery {
+  id?: string;
+  name?: string;
+}
+
+export interface ManagerData {
+  name: string;
+  email: string;
+  bank_id: string;
 }

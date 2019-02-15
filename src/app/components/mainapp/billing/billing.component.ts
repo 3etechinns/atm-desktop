8a9108efd358429dbc7ba03a67a6f2ebe25f1f64
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-15 22:20:13 +0000

    In design process for billing

--- src/app/components/mainapp/billing/billing.component.ts
+++ src/app/components/mainapp/billing/billing.component.ts
@@ -13,53 +13,7 @@ import {
   styleUrls: ['./billing.component.scss']
 })
 export class BillingComponent implements OnInit {
-  elements: Elements;
-  card: StripeElement;
-
-  // optional parameters
-  elementsOptions: ElementsOptions = {
-    locale: 'es'
-  };
-
   constructor(private stripeService: StripeService) {}
 
-  ngOnInit() {
-    this.stripeService.elements(this.elementsOptions).subscribe(elements => {
-      this.elements = elements;
-      // Only mount the element the first time
-      if (!this.card) {
-        this.card = this.elements.create('card', {
-          style: {
-            base: {
-              iconColor: '#666EE8',
-              color: '#31325F',
-              lineHeight: '40px',
-              fontWeight: 300,
-              fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-              fontSize: '18px',
-              '::placeholder': {
-                color: '#CFD7E0'
-              }
-            }
-          }
-        });
-        this.card.mount('#card-element');
-      }
-    });
-  }
-
-  buy() {
-    this.stripeService
-      .createToken(this.card, { name: 'Precious Keyz' })
-      .subscribe(result => {
-        if (result.token) {
-          // Use the token to create a charge or a customer
-          // https://stripe.com/docs/charges
-          console.log(result.token);
-        } else if (result.error) {
-          // Error creating the token
-          console.log(result.error.message);
-        }
-      });
-  }
+  ngOnInit() {}
 }

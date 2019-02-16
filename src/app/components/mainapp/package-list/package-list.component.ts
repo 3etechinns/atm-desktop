bb2997e34e69c4a0b52b38d2e69525b6da0e0718
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-16 02:16:43 +0000

    Added PackageListComponent

---
+++ src/app/components/mainapp/package-list/package-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { BankBillingService } from '@atmhotspot/bank';
+import { Observable, interval } from 'rxjs';
+import { PlanData } from '@atmhotspot/bank/lib/bank.models';
+import {
+  startWith,
+  switchMap,
+  distinctUntilChanged,
+  map,
+  tap
+} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-package-list',
+  templateUrl: './package-list.component.html',
+  styleUrls: ['./package-list.component.scss']
+})
+export class PackageListComponent implements OnInit {
+  @Output() packageClick = new EventEmitter<any>();
+
+  packagesSub: Observable<PlanData[]>;
+
+  constructor(private billingSvc: BankBillingService) {}
+
+  ngOnInit() {
+    this.packagesSub = interval(100000).pipe(
+      startWith(0),
+      switchMap(() => this.billingSvc.getPlans()),
+      tap(data =>
+        data.data.forEach(data => {
+          data.amount = data.amount / 100;
+        })
+      ),
+      map(res =>
+        res.data.sort((a, b) => {
+          return a.amount > b.amount ? 1 : 0;
+        })
+      ),
+      distinctUntilChanged()
+    );
+  }
+
+  click($event) {
+    this.packageClick.emit($event);
+  }
+}
